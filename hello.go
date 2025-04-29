d0b8d48095c8f6ed0ac6c257bafe4a917fe8fbae
Juhaied <juhaied.hossen@northsouth.edu>
2025-04-29 20:34:13 +0600

    Hello world in Go

---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+// This is a simple Go program that prints "Hello, World!" to the console.
+// It demonstrates the basic structure of a Go program, including the package declaration
